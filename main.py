8497d93a4c86a9b0b3c5b9c16a7e8d73d3eb2cb1
Sail128 <r.voort@me.com>
2019-03-11 11:11:27 +0100

    added file checking and basic multithreading

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from tabulate import tabulate as tb
+from multiprocessing.dummy import Pool as ThreadPool
+# from tabulate import tabulate as tb
 
 def getFileList(parent_folder:str):
     dirs = os.listdir(parent_folder)
     FileList = {}
     for dir in dirs:
-        FileList[dir] = os.listdir("{}/{}".format(parent_folder,dir))
-
+        files = os.listdir("{}/{}".format(parent_folder,dir))
+        sets = set()
+        for file in files:
+            if file.endswith(".dat"):
+                keys = file.split("_")
+                sets.add("_".join(keys[0:4]))
+        FileList[dir] = sets
     return FileList
 
+def L2error(setKey):
+    pass
 
 
+def executeParallel(setKey, threads = 8):
+    pool = ThreadPool(threads)
+    result = pool.map(L2error,dirnames)
+    pool.close()
+    pool.join()
+    return setKey, result
 
 def main():
-    print(tb(getFileList("Data")))
+    FileList = getFileList("Data")
+    print(FileList)
 
 
 if __name__ == "__main__":
