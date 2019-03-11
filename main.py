c75f719f8d9d422a215e805642e3935d1dbcc13a
Sail128 <r.voort@me.com>
2019-03-11 11:28:08 +0100

    Correct main

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def getFileList(parent_folder:str):
         FileList[dir] = sets
     return FileList
 
+def phi_exact_calc(x,y):
+    from math import pi as pi
+    phi_exact = np.sin(pi*x) * np.sin(pi*y)
+    return  phi_exact
+
 def L2error(setKey):
     pass
 
@@ -30,18 +35,8 @@ def executeParallel(setKey, threads = 8):
     return setKey, result
 
 def main():
-<<<<<<< HEAD
     FileList = getFileList("Data")
     print(FileList)
-=======
-    print(tb(getFileList("Data")))
-    print("test")
->>>>>>> e508911994e447a9d8efd9cc46a886a0eb91bbef
-
-def phi_exact_calc(x,y):
-    from math import pi as pi
-    phi_exact = np.sin(pi*x) * np.sin(pi*y)
-    return  phi_exact
 
 if __name__ == "__main__":
     main()
