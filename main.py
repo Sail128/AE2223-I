7e89fed469f76813e63e337de81c65a29e7ada0f
Sail128 <r.voort@me.com>
2019-03-11 11:29:00 +0100

    correction for phi

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def getFileList(parent_folder:str):
     return FileList
 
 def phi_exact_calc(x,y):
-    from math import pi as pi
-    phi_exact = np.sin(pi*x) * np.sin(pi*y)
+    phi_exact = np.sin(np.pi*x) * np.sin(np.pi*y)
     return  phi_exact
 
 def L2error(setKey):
