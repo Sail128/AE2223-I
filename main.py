c8bb72f844e97d5ef466cab221c2e1e82ce0376e
Sail128 <r.voort@me.com>
2019-03-11 09:45:20 +0100

    updateje

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tabulate import tabulate as tb
+
+def getFileList(parent_folder:str):
+    dirs = os.listdir(parent_folder)
+    FileList = {}
+    for dir in dirs:
+        FileList[dir] = os.listdir("{}/{}".format(parent_folder,dir))
+
+    return FileList
+
+
 
 
 def main():
-    pass
+    print(tb(getFileList("Data")))
 
 
 if __name__ == "__main__":
